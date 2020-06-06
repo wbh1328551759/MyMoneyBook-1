77007399df69e7798716c973f545f4b0f55c793f
wbh1328551759 <1328551759@qq.com>
2020-06-07 00:34:49 +0800

    创建<Layout />

---
+++ src/components/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nav-wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name:'Layout'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .nav-wrapper {
+    border: 1px solid red;
+    height: 99.7vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .content {
+    border: 1px solid blue;
+    flex-grow: 1;
+    overflow: auto;
+  }
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+    <Layout>
       <p>Money.vue</p>
-    </div>
-    <Nav/>
-  </div>
+    </Layout>
 </template>
 
 <script lang="ts">
@@ -14,16 +11,5 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper {
-    border: 1px solid red;
-    height: 99.7vh;
-    display: flex;
-    flex-direction: column;
-  }
 
-  .content{
-    border:1px solid blue;
-    flex-grow: 1;
-    overflow:auto;
-  }
 </style>
