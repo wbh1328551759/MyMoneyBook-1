cd116ccfbce36a2258515e58f1aeeea4d0f47971
wbh1328551759 <1328551759@qq.com>
2020-06-11 01:35:01 +0800

    点击标签可跳转至编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,15 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag"
+                   v-for="tag in tags"
+                   :key="tag.id"
+                   :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
-      </li>
+      </router-link>
 
-    </ol>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -43,7 +46,7 @@
     font-size: 16px;
     padding-left: 16px;
 
-    > li {
+    > .tag{
       min-height: 44px;
       display: flex;
       align-items: center;
