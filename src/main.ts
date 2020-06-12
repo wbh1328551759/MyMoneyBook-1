89881267ba427337e9cbae5c8dcf1f95d9788d02
wbh1328551759 <1328551759@qq.com>
2020-06-13 00:51:49 +0800

    封装 window.createTag

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,14 @@ Vue.component('Layout',Layout)
 Vue.component('Icon',Icon)
 
 window.tagList = tagListModel.fetch();
-
+window.createTag = (name: string)=>{
+  const message = tagListModel.create(name);
+  if(message === 'duplicated'){
+    window.alert('标签名重复了')
+  }else{
+    window.alert('添加成功')
+  }
+}
 
 new Vue({
   router,
